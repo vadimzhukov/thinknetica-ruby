5cc0c68989af34495b161c0320b0c1e036c98a4e
VadimZhukov <vadim.zhukov@gmail.com>
2022-06-22 23:46:18 +0300

    fix: addintional tests, some refactoring of Station class (auto-arriving when train moved to the station)

--- railway_station/classes.rb
+++ railway_station/classes.rb
@@ -8,6 +8,7 @@ class Station
 
   def receive_train(train)
     @trains_at_station << train
+    puts "Поезд #{train.number} прибыл на станцию #{@title}"
   end
 
   def current_trains
@@ -15,7 +16,7 @@ class Station
   end
 
   def current_trains_by_type(type)
-    @trains_at_station.select {|t| t.type == type}
+    @trains_at_station.select{|t| t.type == type}.map{|x| [x.number, x.type, x.carriage_count]}
   end
 
   def send_train
@@ -48,7 +49,7 @@ end
 
 class Train
   attr_accessor :speed
-  attr_reader :carriage_count, :current_station
+  attr_reader :carriage_count, :current_station, :number, :type
 
   def initialize(number, type, carriage_count)
     @number = number
@@ -82,14 +83,16 @@ class Train
     if direction == "forward" 
       if @current_station != @train_route.all_stations.last
         @current_station = @train_route.all_stations[@train_route.all_stations.index(@current_station) + 1]
+        @current_station.receive_train(self)
       else
         puts "Поезд на конечной станции и не может двигаться вперед"
       end 
     end
 
     if direction == "backward" 
-      if@current_station != @train_route.all_stations.first
+      if @current_station != @train_route.all_stations.first
         @current_station = @train_route.all_stations[@train_route.all_stations.index(@current_station) - 1]
+        @current_station.receive(self)
       else
         puts "Поезд на начальной станции и не может двигаться назад"
       end
@@ -98,25 +101,43 @@ class Train
 end
 
 # tests
+# # Станции
 # balagoe = Station.new("Балагое")
 # moscow = Station.new("Москва")
 # piter = Station.new("Санкт-Петербург")
 # tver = Station.new("Тверь")
 
+# # маршруты
 # moscow_piter = Route.new(moscow, piter)
 # piter_tver = Route.new(piter, tver)
 
+# #доп станции на маршрутах
 # moscow_piter.add_inner_station(balagoe)
-
 # moscow_piter.add_inner_station(tver)
-
 # piter_tver.add_inner_station(balagoe)
 
-# sapsan = Train.new(666, "Грузовой", 10)
-# sapsan.stop
+# # 1 тестовый поезд
+# sapsan = Train.new(666, "Пасажирский", 10)
 # sapsan.change_carriage(1)
-# print sapsan.carriage_count
-
 # sapsan.set_route(moscow_piter)
-# sapsan.move("backward")
-# puts sapsan.current_station.title
+# sapsan.move("forward")
+
+# # 2 тестовый поезд
+# lastochka = Train.new(555, "Пасажирский", 15)
+# lastochka.set_route(piter_tver)
+
+# lastochka.move("forward")
+# lastochka.move("forward")
+
+# #3 тестовый поезд
+# tyagach = Train.new(444, "Грузовой", 40)
+
+# tyagach.set_route(moscow_piter)
+# 5.times do
+#   tyagach.change_carriage(-1)
+# end
+
+# tyagach.move("forward")
+
+# puts "Пассажирские поезда на станции Балагое : #{balagoe.current_trains_by_type("Пасажирский")}"
+# puts "Грузовые поезда на станции Балагое : #{balagoe.current_trains_by_type("Грузовой")}"
