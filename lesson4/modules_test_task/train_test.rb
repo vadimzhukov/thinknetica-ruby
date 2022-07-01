802fee3b1bcc01b4f735fcc7ab8274225a487c25
VadimZhukov <vadim.zhukov@gmail.com>
2022-07-01 10:54:42 +0300

    fix: тестовое задание в train_test.rb теперь полностью рабочее с дочерними классами. Использовал Class-level instance variables

--- lesson4/modules_test_task/train_test.rb
+++ lesson4/modules_test_task/train_test.rb
@@ -1,5 +1,5 @@
 class TestTrain
-  @@instances = 0
+  @instances = 0
 
   def initialize(number, type = :passenger)
     @number = number
@@ -8,13 +8,13 @@ class TestTrain
   end
 
   def self.instances 
-    @@instances
+    @instances
   end
 
   private
   def self.increase_instance
-    @@instances ||= 0
-    @@instances += 1
+    @instances ||= 0
+    @instances += 1
   end
 
   def register_instance
@@ -22,8 +22,31 @@ class TestTrain
   end
 end
 
+class TestPassengerTrain < TestTrain
+  def initialize(number)
+    super(number, :passenger)
+  end
+end
+
+class TestCargoTrain < TestTrain
+  def initialize(number)
+    super(number, :passenger)
+  end
+end
+
+
 tr1 = TestTrain.new("MMM_NUM1")
 tr2 = TestTrain.new("MMM_NUM2")
 tr3 = TestTrain.new("MMM_NUM3")
 
-puts TestTrain.instances
+pas1 = TestPassengerTrain.new("PAS1")
+pas2 = TestPassengerTrain.new("PAS2")
+
+car1 = TestCargoTrain.new("CAR1")
+car2 = TestCargoTrain.new("CAR2")
+car3 = TestCargoTrain.new("CAR3")
+car4 = TestCargoTrain.new("CAR4")
+
+puts "Train: #{TestTrain.instances}"
+puts "PasTrain: #{TestPassengerTrain.instances}"
+puts "CargoTrain: #{TestCargoTrain.instances}"
